099942d351ab161f57daaa776362ffb9529af8bf
Yuhao-Augus-Su <yuhao_career@outlook.com>
2020-12-08 07:53:35 +0800

    change heroku url

--- src/main/java/com/example/cs5610fall2020projectteam23serverjava/controllers/RegisteredUserController.java
+++ src/main/java/com/example/cs5610fall2020projectteam23serverjava/controllers/RegisteredUserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-@CrossOrigin(origins = "https://cs5610-server-java-team23.herokuapp.com/", allowCredentials = "true")
+@CrossOrigin(origins = "https://cs5610-client-react-team23.herokuapp.com/", allowCredentials = "true")
 public class RegisteredUserController {
     @Autowired
     RegisteredUserService service;
